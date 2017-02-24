395e719fbe7b2cb135e7127f4f58126106cecd47
Ravi <210802@CTS.COM>
2017-02-24 12:40:00 +0000

    last

---
+++ src/test/java/pack/GooTest.java
@@ -0,0 +1,21 @@
+package pack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GooTest {
+	
+	@Before
+	public void syout()
+	{
+		System.out.println("before");
+	}
+	
+	@Test	
+	public void test()
+	{
+		System.out.println("tests");
+	}
+
+
+}
